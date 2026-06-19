5ba7d22f850647a3fdcc16f238555259d6307086
Javier Blanco Vila <javier.blanco.vila@example.com>
2026-06-19 17:33:48 +0000

    Initial commit - Django project setup

---
+++ mi_proyecto/urls.py
@@ -0,0 +1,8 @@
+"""Rutas principales del proyecto."""
+
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
---
+++ mi_proyecto/wsgi.py
@@ -0,0 +1,9 @@
+"""WSGI config for mi_proyecto project."""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mi_proyecto.settings')
+
+application = get_wsgi_application()
