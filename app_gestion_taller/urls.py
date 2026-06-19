85d8e93632287122ba24cdae341ef61f79169766
Javier Blanco Vila <javier.blanco.vila@example.com>
2026-06-19 18:17:43 +0000

    Add practica 6 Django forms

--- app_gestion_taller/urls.py
+++ app_gestion_taller/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
 from .views import (
-    buscar_cliente,
     buscar_coche_por_matricula,
     buscar_coches_de_cliente,
     buscar_servicios_de_coche,
+    contacto,
     detalle_cliente,
     lista_clientes,
+    nuevo_cliente,
+    nuevo_coche,
+    nuevo_coche_servicio,
+    nuevo_servicio,
     registrar_cliente,
     registrar_coche,
     registrar_servicio,
@@ -14,6 +18,11 @@ from .views import (
 
 urlpatterns = [
     path('clientes/', lista_clientes, name='lista_clientes'),
+    path('clientes/nuevo/', nuevo_cliente, name='nuevo_cliente'),
+    path('coches/nuevo/', nuevo_coche, name='nuevo_coche'),
+    path('servicios/nuevo/', nuevo_servicio, name='nuevo_servicio'),
+    path('coche-servicio/nuevo/', nuevo_coche_servicio, name='nuevo_coche_servicio'),
+    path('contacto/', contacto, name='contacto'),
     path('clientes/registrar/', registrar_cliente, name='registrar_cliente'),
     path('coches/registrar/', registrar_coche, name='registrar_coche'),
     path('servicios/registrar/', registrar_servicio, name='registrar_servicio'),
